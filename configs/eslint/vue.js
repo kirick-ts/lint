ec9c0aa69012e27f391120a8cba3ed5fc13af649
Daniil Kirichenko <daniil@kirick.me>
2025-10-23 17:46:38 +0200

    Update eslint config for Vue, postinstall script

--- configs/eslint/vue.js
+++ configs/eslint/vue.js
@@ -20,8 +20,32 @@ export const configVue = [
 			'vue/html-self-closing': 'off',
 			'vue/max-attributes-per-line': 'off',
 			'vue/multiline-html-element-content-newline': 'off',
+			'vue/no-multi-spaces': 'off',
+			'vue/no-spaces-around-equal-signs-in-attribute': 'off',
+			'vue/mustache-interpolation-spacing': 'off',
 			'vue/prop-name-casing': ['error', 'snake_case'],
 			'vue/singleline-html-element-content-newline': 'off',
+			// Priority C: Recommended
+			'vue/attributes-order': 'warn',
+			'vue/block-order': [
+				'error',
+				{
+					order: ['template', 'script', 'style'],
+				},
+			],
+			// Uncategorized
+			'vue/block-lang': [
+				'error',
+				{
+					script: {
+						lang: 'ts',
+					},
+				},
+			],
+			'vue/component-api-style': ['error', 'script-setup'],
+			'vue/define-emits-declaration': ['error', 'type-based'],
+			'vue/define-props-declaration': ['error', 'type-based'],
+			'vue/next-tick-style': ['error', 'promise'],
 		},
 	},
 ];
